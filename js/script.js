ea08cca9e228f715140c49a4ebcfc467d54cab3c
mizanurrahman70 <mizanurrahmanofficial70.com>
2024-02-28 15:11:24 +0600

    loading complete

--- js/script.js
+++ js/script.js
@@ -9,6 +9,18 @@ const displayPhon=(phones)=>{
 
 const mainContainer=document.getElementById('phon-container')
 mainContainer.textContent=''
+// show button more then 08
+const show =document.getElementById('show-button') 
+if(phones.length > 8){
+   
+    show.classList.remove('hidden')
+}
+else{
+    show.classList.add('hidden')  
+}
+// display only shows 
+phones=phones.slice(0,8)
+
 phones.forEach(phone => {
     const phonCard=document.createElement('div')
     phonCard.classList=(`card w-96 bg-base-100 shadow-xl`)
@@ -25,13 +37,26 @@ phones.forEach(phone => {
     </div>
   </div>
     `
+
     mainContainer.appendChild(phonCard)
+    sprinner(false)
 });
 }
 // handle search 
 const searchHandle=()=>{
+    sprinner(true)
     const searchFiled= document.getElementById('search-filed');
     const searchText=searchFiled.value ;
     // console.log(searchText)
     loadPhon(searchText)
+}
+const sprinner=(isloading)=>{
+            const sinner =document.getElementById('spiner')
+           if(isloading){
+            sinner.classList.remove('hidden')
+
+           }
+           else{
+            sinner.classList.add('hidden')
+           }
 }
